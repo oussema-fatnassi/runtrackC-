26d3a85d6b9d2f3773cc1dfd406302b8581bce83
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-29 15:16:06 +0200

    Jobs done until job4

---
+++ Jour01/Job01/job1.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+char c = '\x01';            // Its a character
+short int p = 10;           // Its a short integer
---
+++ Jour01/Job03/job3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{   int i = 10;
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Hello World!" << std::endl;
+    }
+    return 0;
+}
