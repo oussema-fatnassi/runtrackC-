0972e19039ec1d7084aaaad329002a3befc02798
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-29 15:27:36 +0200

    Job05 finished

---
+++ Jour01/Job05/job05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int a;
+int b;
+
+int main(){
+    std::cout << "Enter a number: ";
+    std::cin >> a;
+    std::cout << "Enter another number: ";
+    std::cin >> b;
+    std::cout << "The multiplication of " << a << " and " << b << " is " << a*b << std::endl;
+    return 0;
+}
