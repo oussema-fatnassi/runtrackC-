00ab3fde7f051272830d92a88eec8708bbdb449c
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-05-02 16:18:46 +0200

    Job2: swap values with pointers

---
+++ Jour04/Job02/job2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+    swap(&a, &b);
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
