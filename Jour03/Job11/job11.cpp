1db0772041cc28b0436706b899932c1fbc643480
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-05-02 11:52:17 +0200

    Job11: sum of even numbers of an array

--- Jour03/Job11/job11.cpp
+++ Jour03/Job11/job11.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int table[7];
+int arrayNumbers[7];
 int sum;
 
 int main(){
     cout << "Enter 7 numbers: ";
     for (int i=0; i<7; i++){
-        cin >> table[i];
+        cin >> arrayNumbers[i];
     }
 
     for (int i=0; i<7; i++){
-        if (table[i] % 2 == 0){
-            sum += table[i];
+        if (arrayNumbers[i] % 2 == 0){
+            sum += arrayNumbers[i];
         }
     }
     cout << "The sum of the even numbers is: " << sum << endl;
